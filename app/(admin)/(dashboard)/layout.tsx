693a96e3ac2884b4f862bf4d8eafa7da654f64a1
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-06-25 22:09:16 +0200

    🔒️ Add authentication

---
+++ app/(admin)/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import AuthButton from "@/components/AuthButton";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function ProtectedLayout() {
+    const supabase = createClient();
+
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+
+    if (!user) {
+        return redirect("/login");
+    }
+
+    return <div className="flex-1 w-full flex flex-col gap-20 items-center"></div>;
+}
