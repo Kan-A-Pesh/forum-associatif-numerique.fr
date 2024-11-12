53876fc1610ba7e48286cce989750f9ae2091fdb
Kan-A-Pesh <michelm.gaming@gmail.com>
2024-11-12 21:17:15 +0100

    chore: added public files and fixed ci

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
+    experimental: {
+        outputStandalone: true,
+    },
     images: {
         remotePatterns: [
             {
